9c1bc1004f5dd23c28e8377e44ea32b877c35ecd
Andrew Janke <a.janke@gmail.com>
2016-06-03 10:37:39 +1000

    updates to R script

--- analysis-pipe/04-Rstats02.R
+++ analysis-pipe/04-Rstats02.R
@@ -1,10 +1,6 @@
 #! /usr/bin/Rscript
-#
+
 ### Load necessary libraries (all CRAN libraries can be acquired using install.packages)
-##
-# > install.packages("tclust")
-
-
 library(compiler)
 library(AnalyzeFMRI)
 library(MASS)
@@ -17,20 +13,17 @@ library(speedglm)
 library(pracma)
 library(lattice)
 library(tclust)
+library(dynamicTreeCut)
 
 ### Enable Just In Time Compiling, for ????MAYBE???? more speed.
 enableJIT(3)
 
 # SPLINE FITTING --------------------------------------
 
-scape_dir <- "/data/home/uqajank2/data/NIF053-ZFISH-SCAPE/SCAPE-MNC2/20150628_5dpf_H2BS_CON_LR_F11/03-chunk/"
-
 ### Fit Splines to the time series in each of the slice files (ss loops over slices)
 for (ss in 1:150) {
   ### Set where the slices are
-  setwd(scape_dir)
-  #setwd("C:/Users/uqhngu10/Dropbox/Collaborations/Jeremy Ullmann/Zebrafish_260216/for-hien/20150628_5dpf_H2BS_CON_LR_F11/03-chunk/NII")
-  
+  setwd("~/Dropbox/Collaborations/Jeremy Ullmann/Zebrafish_260216/for-hien/20150628_5dpf_H2BS_CON_LR_F11/03-chunk/NII")  
   ### Declare what files to load in
   ### Remove time points that do not look stationary
   file_list <- list.files()
@@ -38,10 +31,11 @@ for (ss in 1:150) {
   file_number <- as.numeric(substr(file_list,7,12))
   
   ### These lines are kind of data dependent. I've determined what works for F11, F03, and F04.
-  # file_list <- file_list[which(file_number>2000 & file_number<5000)] # F11
-  file_list <- file_list[which(file_number>500 & file_number<=3300)] # F03 and F04
-  file_number <- as.numeric(substr(file_list,7,12)) - 500 # - 500 is F03 and F04 dependent, for F11 it would be - 2000.
+  file_list <- file_list[which(file_number>2000 & file_number<5000)] # F11
+  # file_list <- file_list[which(file_number>500 & file_number<=3300)] # F03 and F04
+  file_number <- as.numeric(substr(file_list,7,12)) - 2000 # - 500 is F03 and F04 dependent, for F11 it would be - 2000.
   
+  file_number.old <- file_number
   max_file <- max(file_number)
   file_number <- (file_number-1)*340 + ss
   
@@ -84,19 +78,13 @@ for (ss in 1:150) {
   Basis <- create.bspline.basis(c(0,(max_file-1)*340+150),
                                 nbasis=Basis_number)
   BS <- eval.basis(file_number,Basis)
-  
+
   ### Fit B-spline to all time series
-  coeff_mat <- matrix(NA,170*70,Basis_number)
-  count <- 0
-  for (ii in 1:(170*70)) {
-    LM <- fastLmPure(BS,full_mat[ii,])
-    # LM <- speedlm.fit(full_mat[ii,],BS)
-    coeff_mat[ii,] <- LM$coefficients
-    print(c('filtering',ss,ii))
-  }
+  FD <- smooth.basisPar(file_number,t(full_mat),Basis)
+  coeff_mat <- as.matrix(t(FD$fd$coefs))
   
   ### Set file location for where you want to save the results
-  setwd(scape_dir)
+  setwd("~/Dropbox/Collaborations/Jeremy Ullmann/Zebrafish_260216/for-hien/20150628_5dpf_H2BS_CON_LR_F11/03-chunk")  
   
   ### Save the results in the format coeff_mat_(SLICE NUMBER).rdata
   save(coeff_mat,file=paste('coeff_mat_',ss,'.rdata',sep=''))
@@ -106,7 +94,7 @@ for (ss in 1:150) {
 # TRIMMED K-MEANS CLUSTERING ------------------------
 
 ### Set file location where the spline fitting results are saved
-setwd(scape_dir)
+setwd("~/Dropbox/Collaborations/Jeremy Ullmann/Zebrafish_260216/for-hien/20150628_5dpf_H2BS_CON_LR_F11/03-chunk")  
 
 ### Load all of the coefficients and put them into a BIG matrix
 load(paste('coeff_mat_',1,'.rdata',sep=''))
@@ -139,11 +127,11 @@ scale_mat <- scale(big_mat)
 rm(big_mat)
 
 ### Set a location for where BIC results should be saved
-setwd(scape_dir)
+setwd("~/Dropbox/Collaborations/Jeremy Ullmann/Zebrafish_260216/for-hien/20150628_5dpf_H2BS_CON_LR_F11")  
 
-### Compute BIC over a range of K (here 2--80)
+### Compute BIC over a range of K (here 50--100)
 BIC_val <- c()
-for (kk in 2:80) {
+for (kk in 50:100) {
   # Conduct timing while computing BIC values
   TIME <- proc.time()
   BIC_val[kk] <- tmeansBIC(tkmeans(x=scale_mat,k=kk,alpha=.9,nstart=3,iter.max=10),scale_mat)
@@ -155,7 +143,8 @@ for (kk in 2:80) {
 }
 
 ### Get the optimal K and computer clustering under optimal K
-comp <- which.min(BIC_val,na.rm=T)
+comp <- which.min(BIC_val)
+
 ### Cluster using the optimal value for K
 clustering <- tkmeans(x=scale_mat,k=comp,alpha=.9,nstart=5,iter.max=10)
 
@@ -183,8 +172,9 @@ image.plot(image_hold[75,,],col=tim.colors(comp))
 
 ### Obtain the cluster mean time series
 # Reload Graphing Parameters (These values are specific to F03 and F04)
-max_file <- 3300
-file_number <- 501:3300-500
+file_number <- file_number.old
+max_file <- max(file_number)
+file_number <- (file_number-1)*340 + ss
 Basis_number <- 200
 Basis <- create.bspline.basis(c(0,(max_file-1)*340+150),
                               nbasis=Basis_number)
@@ -234,6 +224,7 @@ for (c1 in 1:comp) {
 # Plot Correlation matrix
 image.plot(CORR)
 
+
 ### Create a Mean value image (using the predicted mean values from MEAN_PRED)
 which_clust <- 6
 mean_image <- array(NA,c(150,170,70))
@@ -249,3 +240,161 @@ for (ss in 1:150)
 }
 # Plot the mean image at slice 90
 image.plot(1:170,1:70,mean_image[90,,],col=grey.colors(100,0,1))
+
+
+# Graphing --------------------------------------------
+# Make New directory and set to new directory for graphs
+dir.create('Graphs_and_results')
+setwd('./Graphs_and_results')
+
+### First set of graphs
+# Graph clustering on every 5th slice
+for (slice in seq(5,150,by=5)) {
+  pdf(paste('Clustering_Slice_',slice,'.pdf',sep=''),paper='a4r')
+  # pdf(paste('Clustering_Slice_',slice,'.pdf',sep=''),paper='a4r')
+  ### Plot Clustering Image
+  image_hold <- array(NA,c(150,170,70))
+  count <- 0
+  for (ss in 1:150) {
+    for (ii in 1:170) {
+      for (jj in 1:70) {
+        count <- count + 1
+        image_hold[ss,ii,jj] <- clustering_cluster[count]
+      }
+    } 
+  }
+  
+  image.plot(image_hold[slice,,],col=tim.colors(comp))
+  dev.off()
+}
+
+# Graph Mean Slices on every 5th slice
+for (slice in seq(5,150,by=5)) {
+  pdf(paste('Mean_Slice_',slice,'.pdf',sep=''),paper='a4r')
+  # jpeg(paste('Mean_Slice_',slice,'.jpeg',sep=''))
+  ### Plot Mean Image
+  mean_image <- array(NA,c(150,170,70))
+  count <- 0
+  for (ss in 1:150)
+  {
+    for (ii in 1:170) {
+      for (jj in 1:70) {
+        count <- count + 1
+        mean_image[ss,ii,jj] <- MEAN_PRED[clustering_cluster[count]]
+      }
+    }
+  }
+  image.plot(1:170,1:70,mean_image[slice,,],col=grey.colors(100,0,1))
+  dev.off()
+  
+}
+
+# Graph the location of clusters on slice 75
+for (cc in 1:comp) {
+  pdf(paste('Location_on_Slice_75_Cluster_',cc,'.pdf',sep=''),paper='a4r')
+  # jpeg(paste('Location_on_Slice_75_Cluster_',cc,'.jpeg',sep=''))
+  image.plot(1:170,1:70,mean_image[75,,],col=grey.colors(100,0,1))
+  count <- 0
+  for (ss in 1:150) {
+    for (ii in 1:170) {
+      for (jj in 1:70) {
+        count <- count + 1
+        if (clustering_cluster[count]==cc & ss==75) {
+          points(ii,jj,pch=15,cex=1,col='green')
+        }
+      }
+    }  
+  }
+  dev.off()
+}
+
+# Graph the location of clusters on slice 50
+for (cc in 1:comp) {
+  pdf(paste('Location_on_Slice_50_Cluster_',cc,'.pdf',sep=''),paper='a4r')
+  # jpeg(paste('Location_on_Slice_75_Cluster_',cc,'.jpeg',sep=''))
+  image.plot(1:170,1:70,mean_image[50,,],col=grey.colors(100,0,1))
+  count <- 0
+  for (ss in 1:150) {
+    for (ii in 1:170) {
+      for (jj in 1:70) {
+        count <- count + 1
+        if (clustering_cluster[count]==cc & ss==50) {
+          points(ii,jj,pch=15,cex=1,col='green')
+        }
+      }
+    }  
+  }
+  dev.off()
+}
+
+# Graph the location of clusters on slice 100
+for (cc in 1:comp) {
+  pdf(paste('Location_on_Slice_100_Cluster_',cc,'.pdf',sep=''),paper='a4r')
+  # jpeg(paste('Location_on_Slice_75_Cluster_',cc,'.jpeg',sep=''))
+  image.plot(1:170,1:70,mean_image[100,,],col=grey.colors(100,0,1))
+  count <- 0
+  for (ss in 1:150) {
+    for (ii in 1:170) {
+      for (jj in 1:70) {
+        count <- count + 1
+        if (clustering_cluster[count]==cc & ss==100) {
+          points(ii,jj,pch=15,cex=1,col='green')
+        }
+      }
+    }  
+  }
+  dev.off()
+}
+
+# Graph the Mean functions
+for (cc in 1:comp) {
+  pdf(paste('Cluster_Mean_Function_',cc,'.pdf',sep=''),paper='a4r')
+  # pdf(paste('Cluster_Mean_Function_',cc,'.pdf',sep=''),paper='a4r')
+  plot(seq(1,((max_file-1)*340+150),1000),PRED[cc,],type='l',xlab='Time',ylab='signal',main=cc)
+  dev.off()
+}
+
+# Plot the Correlation matrix
+pdf('Correlation_matrix.pdf',paper='a4r')
+image.plot(1:comp,1:comp,CORR)
+dev.off()
+
+
+# Hierachical Clustering ------------------------------
+# Make a distance metric
+DIST <- as.dist(1-t(CORR))
+# Conduct hierachical clustering
+HCLUST <- hclust(DIST)
+
+# Make tree cut using dynamicTreeCut
+CUT <- cutreeDynamicTree(HCLUST,minModuleSize=1)
+
+# Plot dendrogram
+pdf('Dendrogram_clusters.pdf',width=30,height=10)
+plot(HCLUST)
+rect.hclust(HCLUST,max(CUT),border=rainbow(max(CUT)))
+dev.off()
+
+### Plot the 10 HCLust Cluster Means
+for (ii in 1:max(CUT)) {
+  pdf(paste('HCLUST_',ii,'.pdf',sep=''),paper='a4r')
+  plot(c(1,((max_file-1)*340+150)),c(min(PRED),max(PRED)),
+       type='n',xlab='Time',ylab='signal',main=ii)
+  for (ss in 1:comp) {
+    if (CUT[ss]==ii) {
+      lines(seq(1,((max_file-1)*340+150),1000),PRED[ss,],col=tim.colors(comp)[ss])
+    }
+  }
+  dev.off()
+}
+
+# Plot Frequency Histogram
+pdf('Frequency_of_clusters.pdf',paper='a4r')
+plot(table(clustering_cluster),xlab='cluster',ylab='Frequency')
+dev.off()
+
+# Plot K means by HCLUST reference
+pdf('Cluster_by_HCLUST.pdf',paper='a4r')
+plot(CUT,col=tim.colors(86),xlim=c(0,comp+1),ylim=c(0,8),xlab='Cluster',ylab='HCLUST')
+grid()
+dev.off()
